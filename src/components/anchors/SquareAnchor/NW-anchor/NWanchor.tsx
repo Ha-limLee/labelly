1b3c60fb101750c838826399ab189bf6f92a09f1
LeeHalim <leehalim09@gmail.com>
2022-08-10 21:24:44 +0900

    resolved conflicts

--- src/components/anchors/SquareAnchor/NW-anchor/NWanchor.tsx
+++ src/components/anchors/SquareAnchor/NW-anchor/NWanchor.tsx
@@ -1,19 +1,7 @@
-import { useLabelListDispatch, useLabelListState } from "contexts/LabelListContext";
-import React from "react";
 import { SquareAnchorWithDrag } from "../common";
 
 const NWanchor = ({id, left, top}: {id: number, left: number, top: number}) => {
-    const labelList = useLabelListState();
-    const labelListDispatch = useLabelListDispatch();
-    const label = labelList[id];
-
-    const onBetween = (e: React.MouseEvent) => {
-        const diff = Math.abs(label.top - e.pageY);
-        const height = Math.abs(label.height - diff);
-        labelListDispatch({type: 'modify', id: id, height: height});
-    };
-    
-    return SquareAnchorWithDrag({between: onBetween})({left: left, top: top, style: {cursor: 'nw-resize'}});
+    return SquareAnchorWithDrag({})({left: left, top: top, style: {cursor: 'nw-resize'}});
 };
 
 export default NWanchor;
