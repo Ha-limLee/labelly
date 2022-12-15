af37d71aa93be97e4f96b0ed518019e5c564e7a6
LeeHalim <leehalim09@gmail.com>
2022-12-15 22:56:48 +0900

    changed mode context into mode slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import labelGroupReducer from '../features/Label/LabelGroupSlice';
+import modeReducer from '../features/mode/modeSlice';
 
 export const store = configureStore({
     reducer: {
         labelGroup: labelGroupReducer,
+        mode: modeReducer,
     },
 });
 
---
+++ src/features/mode/modeSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "app/store";
+import { UnionType } from "typescript";
+
+export type LABEL_MODE = "SELECT" | "CREATE";
+
+interface ModeState {
+    value: LABEL_MODE;
+}
+
+const initialState: ModeState = {
+    value: "SELECT",
+}
+
+export const modeSlice = createSlice({
+    name: "mode",
+    initialState,
+    reducers: {
+        set: (state, { payload }: PayloadAction<LABEL_MODE>) => {
+            state.value = payload;
+        },
+    }
+});
+
+export const { set } = modeSlice.actions;
+
+export const selectMode = (state: RootState) => state.mode.value;
+
+export default modeSlice.reducer;
